ec3fe299f298df8d0c68e437d881ed5fdaa06454
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2016-09-22 01:09:46 +0100

    deployment: move dist folder into deployment

--- deployment/scripts/AfterInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove deployment files, copied by the standard copying process (doesn't support exclusions unfortunately)
-rm -f /var/www/html/appspec.yml
-rm -rf /var/www/html/scripts
-
-exit 0
-
